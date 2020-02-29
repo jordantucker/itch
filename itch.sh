bebfa570391d7d0873318932dc651cae183b2e9b
Jordan Tucker <jordantucker@gmail.com>
2020-02-29 13:36:13 -0600

    gzip logs

--- itch.sh
+++ itch.sh
@@ -3,7 +3,7 @@
 SCRATCH_ROOT=/uscmst1b_scratch/lpc1/3DayLifetime/$USER/itch
 SCRATCH_LOG=~/.itch.log
 
-mv ${SCRATCH_LOG} ${SCRATCH_LOG}.last
+mv ${SCRATCH_LOG}.gz ${SCRATCH_LOG}.last.gz
 touch ${SCRATCH_LOG}
 
 find ${SCRATCH_ROOT} -type f -print0 | 
@@ -13,4 +13,6 @@ while IFS= read -r -d '' fn; do
 done
 
 echo scratch from $(hostname) at $(date) '<br>'
+gunzip ${SCRATCH_LOG}.last.gz
 diff -q ${SCRATCH_LOG}.last ${SCRATCH_LOG}
+gzip ${SCRATCH_LOG}.last ${SCRATCH_LOG}
